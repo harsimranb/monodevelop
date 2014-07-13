14323dc8d1490baae80a7aded12f68e55d0e1d26
Harsimran Singh Bath <harsimranbath@gmail.com>
2014-07-13 13:07:50 -0700

    [CSS] Created new addin.  Added Css Empty file template.

---
+++ main/src/addins/MonoDevelop.CssSupport/Properties/AddinInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"MonoDevelop.CssSupport", 
+	Namespace = "MonoDevelop.CssSupport",
+	Version = "1.0"
+)]
+
+[assembly:AddinName ("Css Support")]
+[assembly:AddinCategory ("Web Development")]
+[assembly:AddinDescription ("Support for Css documents.")]
+[assembly:AddinAuthor ("Harsimran Bath")]
+
+[assembly:AddinDependency ("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency ("::MonoDevelop.Ide", MonoDevelop.BuildInfo.Version)]
