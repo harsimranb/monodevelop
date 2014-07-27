2920ab5a284271c4c77d0992944b99752bdfc418
Harsimran Singh Bath <harsimranbath@gmail.com>
2014-07-27 03:42:34 -0700

    [JavaScript] Removed MemberReference.cs and updated logic for GotoDeclaration.

--- main/src/addins/MonoDevelop.JavaScriptBinding/Refactoring/GotoDeclarationHandler.cs
+++ main/src/addins/MonoDevelop.JavaScriptBinding/Refactoring/GotoDeclarationHandler.cs
@@ -24,16 +24,80 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using ICSharpCode.NRefactory.TypeSystem;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Core;
 using MonoDevelop.Ide;
+using MonoDevelop.Ide.FindInFiles;
+using MonoDevelop.Ide.TypeSystem;
+using MonoDevelop.Projects;
+using MonoDevelop.JavaScript;
 
-namespace Refactoring
+namespace MonoDevelop.JavaScript
 {
-	public class GotoDeclarationHandler
+	public class GotoDeclarationHandler : CommandHandler
 	{
-		void openDeclaration (string filename, int beginLine, int beginColumn)
+		protected override void Run (object dataItem)
 		{
-			IdeApp.Workbench.OpenDocument (filename, null, beginLine, beginColumn);
+			if (dataItem != null)
+				reportDeclarations (dataItem.ToString ());
+		}
+
+		static void reportDeclarations (string term)
+		{
+			var currentProject = IdeApp.ProjectOperations.CurrentSelectedProject;
+			using (var monitor = IdeApp.Workbench.ProgressMonitors.GetSearchProgressMonitor (true, true)) {
+				IProjectContent context = TypeSystemService.GetProjectContext (currentProject);
+				var jsProjectContent = context as JSUpdateableProjectContent;
+				if (jsProjectContent != null) {
+					monitor.ReportResults (FindReferencesByProject (jsProjectContent, term));
+				}
+			}
+		}
+
+		public static IEnumerable<SearchResult> FindReferencesByProject (JSUpdateableProjectContent project, string query)
+		{
+			var results = new List<SearchResult> ();
+			foreach (var item in project.DocumentsCache) {
+				recurseJSAst (query, item.SimpleAst, ref results);
+			}
+			return results;
+		}
+
+		static void recurseJSAst (string query, SimpleJSAst ast, ref List<SearchResult> results)
+		{
+			foreach (var node in ast.AstNodes) {
+				var varData = node as JSVariableDeclaration;
+				if (varData != null && !string.IsNullOrWhiteSpace (varData.Name) && varData.Name == query) {
+					results.Add (createSearchResult(varData.Filename, varData.SourceCodePosition.StartLine, varData.SourceCodePosition.StartColumn, varData.Name));
+					continue;
+				}
+		
+				var funcData = node as JSFunctionStatement;
+				if (funcData != null && funcData.Name == query && funcData.Name == query) {
+					results.Add (createSearchResult(funcData.Filename, funcData.SourceCodePosition.StartLine, funcData.SourceCodePosition.StartColumn, funcData.Name));
+					continue;
+				}
+			}
+		}
+			
+		static SearchResult createSearchResult(string filename, int beginLine, int beginColumn, string name)
+		{
+			var provider = new MonoDevelop.Ide.FindInFiles.FileProvider (filename);
+			var doc = new Mono.TextEditor.TextDocument ();
+			doc.Text = provider.ReadString ();
+			int position = doc.LocationToOffset (beginLine, beginColumn);
+			while (position + name.Length < doc.TextLength) {
+				if (doc.GetTextAt (position, name.Length) == name)
+					break;
+				position++;
+			}
+
+			return new SearchResult (provider, position, name.Length);
 		}
 	}
 }
-
